49afecd83bd1b68eba0cde46791af54e3b469559
NihatCelik <nc.nihatcelik@hotmail.com>
2019-07-19 11:26:02 +0200

    First Commit

--- GetRestaurantsFromMyMenu/Form1.Designer.cs
+++ GetRestaurantsFromMyMenu/Form1.Designer.cs
@@ -28,13 +28,225 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnOpenInBrowser = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudFinish = new System.Windows.Forms.NumericUpDown();
+            this.nudStart = new System.Windows.Forms.NumericUpDown();
+            this.btnReadRestaurantLinks = new System.Windows.Forms.Button();
+            this.btnSaveRestaurantLinks = new System.Windows.Forms.Button();
+            this.btnReadCSV = new System.Windows.Forms.Button();
+            this.btnScanPostalCodes = new System.Windows.Forms.Button();
+            this.btnWriteCSV = new System.Windows.Forms.Button();
+            this.btnReadPostalCodes = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnScanRestaurantsFromLinks = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFinish)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudStart)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnOpenInBrowser);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.nudFinish);
+            this.panel1.Controls.Add(this.nudStart);
+            this.panel1.Controls.Add(this.btnReadRestaurantLinks);
+            this.panel1.Controls.Add(this.btnSaveRestaurantLinks);
+            this.panel1.Controls.Add(this.btnReadCSV);
+            this.panel1.Controls.Add(this.btnScanPostalCodes);
+            this.panel1.Controls.Add(this.btnScanRestaurantsFromLinks);
+            this.panel1.Controls.Add(this.btnWriteCSV);
+            this.panel1.Controls.Add(this.btnReadPostalCodes);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 74);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnOpenInBrowser
+            // 
+            this.btnOpenInBrowser.Location = new System.Drawing.Point(410, 40);
+            this.btnOpenInBrowser.Name = "btnOpenInBrowser";
+            this.btnOpenInBrowser.Size = new System.Drawing.Size(131, 23);
+            this.btnOpenInBrowser.TabIndex = 10;
+            this.btnOpenInBrowser.Text = "Open In Browser";
+            this.btnOpenInBrowser.UseVisualStyleBackColor = true;
+            this.btnOpenInBrowser.Click += new System.EventHandler(this.btnOpenInBrowser_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(325, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "-";
+            // 
+            // nudFinish
+            // 
+            this.nudFinish.Location = new System.Drawing.Point(335, 41);
+            this.nudFinish.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.nudFinish.Name = "nudFinish";
+            this.nudFinish.Size = new System.Drawing.Size(69, 20);
+            this.nudFinish.TabIndex = 9;
+            // 
+            // nudStart
+            // 
+            this.nudStart.Location = new System.Drawing.Point(256, 41);
+            this.nudStart.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.nudStart.Name = "nudStart";
+            this.nudStart.Size = new System.Drawing.Size(69, 20);
+            this.nudStart.TabIndex = 7;
+            // 
+            // btnReadRestaurantLinks
+            // 
+            this.btnReadRestaurantLinks.Location = new System.Drawing.Point(410, 11);
+            this.btnReadRestaurantLinks.Name = "btnReadRestaurantLinks";
+            this.btnReadRestaurantLinks.Size = new System.Drawing.Size(131, 23);
+            this.btnReadRestaurantLinks.TabIndex = 3;
+            this.btnReadRestaurantLinks.Text = "Read Restaurant Links";
+            this.btnReadRestaurantLinks.UseVisualStyleBackColor = true;
+            this.btnReadRestaurantLinks.Click += new System.EventHandler(this.btnReadRestaurantLinks_Click);
+            // 
+            // btnSaveRestaurantLinks
+            // 
+            this.btnSaveRestaurantLinks.Location = new System.Drawing.Point(256, 11);
+            this.btnSaveRestaurantLinks.Name = "btnSaveRestaurantLinks";
+            this.btnSaveRestaurantLinks.Size = new System.Drawing.Size(148, 23);
+            this.btnSaveRestaurantLinks.TabIndex = 2;
+            this.btnSaveRestaurantLinks.Text = "Save Restaurant Links";
+            this.btnSaveRestaurantLinks.UseVisualStyleBackColor = true;
+            this.btnSaveRestaurantLinks.Click += new System.EventHandler(this.btnSaveRestaurantLinks_Click);
+            // 
+            // btnReadCSV
+            // 
+            this.btnReadCSV.Location = new System.Drawing.Point(134, 40);
+            this.btnReadCSV.Name = "btnReadCSV";
+            this.btnReadCSV.Size = new System.Drawing.Size(116, 23);
+            this.btnReadCSV.TabIndex = 6;
+            this.btnReadCSV.Text = "Read CSV";
+            this.btnReadCSV.UseVisualStyleBackColor = true;
+            this.btnReadCSV.Click += new System.EventHandler(this.btnReadCSV_Click);
+            // 
+            // btnScanPostalCodes
+            // 
+            this.btnScanPostalCodes.Location = new System.Drawing.Point(134, 11);
+            this.btnScanPostalCodes.Name = "btnScanPostalCodes";
+            this.btnScanPostalCodes.Size = new System.Drawing.Size(116, 23);
+            this.btnScanPostalCodes.TabIndex = 1;
+            this.btnScanPostalCodes.Text = "Scan Postal Codes";
+            this.btnScanPostalCodes.UseVisualStyleBackColor = true;
+            this.btnScanPostalCodes.Click += new System.EventHandler(this.btnScanPostalCodes_Click);
+            // 
+            // btnWriteCSV
+            // 
+            this.btnWriteCSV.Location = new System.Drawing.Point(12, 40);
+            this.btnWriteCSV.Name = "btnWriteCSV";
+            this.btnWriteCSV.Size = new System.Drawing.Size(116, 23);
+            this.btnWriteCSV.TabIndex = 5;
+            this.btnWriteCSV.Text = "Write CSV";
+            this.btnWriteCSV.UseVisualStyleBackColor = true;
+            this.btnWriteCSV.Click += new System.EventHandler(this.btnWriteCSV_Click);
+            // 
+            // btnReadPostalCodes
+            // 
+            this.btnReadPostalCodes.Location = new System.Drawing.Point(12, 11);
+            this.btnReadPostalCodes.Name = "btnReadPostalCodes";
+            this.btnReadPostalCodes.Size = new System.Drawing.Size(116, 23);
+            this.btnReadPostalCodes.TabIndex = 0;
+            this.btnReadPostalCodes.Text = "Read Postal Codes";
+            this.btnReadPostalCodes.UseVisualStyleBackColor = true;
+            this.btnReadPostalCodes.Click += new System.EventHandler(this.btnReadPostalCodes_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(246, 74);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(554, 376);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.Url = new System.Uri("https://www.mymenu.be", System.UriKind.Absolute);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 74);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(246, 376);
+            this.panel2.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(246, 376);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnScanRestaurantsFromLinks
+            // 
+            this.btnScanRestaurantsFromLinks.Location = new System.Drawing.Point(547, 11);
+            this.btnScanRestaurantsFromLinks.Name = "btnScanRestaurantsFromLinks";
+            this.btnScanRestaurantsFromLinks.Size = new System.Drawing.Size(164, 23);
+            this.btnScanRestaurantsFromLinks.TabIndex = 4;
+            this.btnScanRestaurantsFromLinks.Text = "Scan Restaurants From Links";
+            this.btnScanRestaurantsFromLinks.UseVisualStyleBackColor = true;
+            this.btnScanRestaurantsFromLinks.Click += new System.EventHandler(this.btnScanRestaurantsFromLinks_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Get Restaurants From MyMenu";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFinish)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudStart)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnReadPostalCodes;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnScanPostalCodes;
+        private System.Windows.Forms.Button btnSaveRestaurantLinks;
+        private System.Windows.Forms.Button btnReadRestaurantLinks;
+        private System.Windows.Forms.Button btnReadCSV;
+        private System.Windows.Forms.Button btnWriteCSV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudFinish;
+        private System.Windows.Forms.NumericUpDown nudStart;
+        private System.Windows.Forms.Button btnOpenInBrowser;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnScanRestaurantsFromLinks;
     }
 }
 
--- GetRestaurantsFromMyMenu/Form1.cs
+++ GetRestaurantsFromMyMenu/Form1.cs
@@ -1,20 +1,200 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Windows.Forms;
+using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
 
 namespace GetRestaurantsFromMyMenu
 {
     public partial class Form1 : Form
     {
+        string baseLink = "https://www.mymenu.be/";
+        string postalCodesFileName = "PostalCodes.txt";
+        string restaurantLinksFileName = "RestaurantLinks.txt";
+        List<string> listPostalCodes;
+        List<Restaurants> listRestaurants;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnReadPostalCodes_Click(object sender, EventArgs e)
+        {
+            listPostalCodes = new List<string>();
+            if (!File.Exists(postalCodesFileName))
+            {
+                File.Create(postalCodesFileName);
+                return;
+            }
+
+            StreamReader sr = new StreamReader(postalCodesFileName);
+            while (!sr.EndOfStream)
+            {
+                listPostalCodes.Add(sr.ReadLine());
+            }
+            sr.Close();
+            MessageBox.Show("File Read.");
+        }
+
+        private async void btnScanPostalCodes_Click(object sender, EventArgs e)
+        {
+            listRestaurants = new List<Restaurants>();
+            IWebDriver driver = new ChromeDriver();//Google Chrome’un açılması için yapıyoruz. Aynı zamanda driver diye nesne tanımlamış olduk. Bu nesne üzerinden işlemleri yapacağız.
+            driver.Manage().Window.Maximize();
+
+            for (int i = 0; i < listPostalCodes.Count; i++)
+            {
+                this.Text = (i + 1).ToString() + "/" + listPostalCodes.Count.ToString();
+                git:
+                try
+                {
+                    driver.Navigate().GoToUrl(baseLink);
+                    Wait(driver);
+                    string postalCode = listPostalCodes[i];
+                    IWebElement txtInput = driver.FindElement(By.TagName("input"));
+                    txtInput.Clear();
+                    txtInput.SendKeys(postalCode);
+                    var element = driver.FindElement(By.CssSelector("div[class='uix-button uix-orange-background']"));
+                    element.Click();
+                    Wait(driver);
+
+                    if (driver.Url == baseLink) continue;
+                    HtmlDocument web = new HtmlDocument();
+                    web.LoadHtml(driver.PageSource);
+
+                    webBrowser1.Navigate(driver.Url);
+                    while (webBrowser1.ReadyState != WebBrowserReadyState.Complete)
+                    {
+                        Application.DoEvents();
+                    }
+                    var elementRestaurants = webBrowser1.Document.GetElementsByTagName("a")
+                              .Cast<HtmlElement>()
+                              .Where(f => !String.IsNullOrEmpty(f.GetAttribute("data-role"))
+                                  && f.GetAttribute("data-role").Contains("restaurants"));
+
+                    foreach (HtmlElement item in elementRestaurants)
+                    {
+                        string link = item.OuterHtml;
+                        link = link.Substring(link.IndexOf("href") + 7);
+                        link = link.Substring(0, link.IndexOf("data-role") - 2);
+                        listRestaurants.Add(new Restaurants { PostalCode = postalCode, URL = baseLink + link });
+                    }
+
+                }
+                catch (Exception)
+                {
+                    Wait(driver);
+                    goto git;
+                }
+            }
+            MessageBox.Show("Scan Finished.");
+        }
+
+        private static void Wait(IWebDriver driver)
+        {
+            Thread.Sleep(1500);
+            new WebDriverWait(driver, TimeSpan.FromSeconds(3000)).Until(
+                  d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
+        }
+
+        private void btnSaveRestaurantLinks_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(restaurantLinksFileName);
+            foreach (var item in listRestaurants)
+            {
+                sw.WriteLine(item.PostalCode + ";" + item.URL);
+            }
+            sw.Close();
+        }
+
+        private void btnReadRestaurantLinks_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listRestaurants = new List<Restaurants>();
+            StreamReader sr = new StreamReader(restaurantLinksFileName);
+            while (!sr.EndOfStream)
+            {
+                string lines = sr.ReadLine();
+                string[] line = Regex.Split(lines, ";");
+                listRestaurants.Add(new Restaurants { PostalCode = line[0], URL = line[1] });
+                listBox1.Items.Add(lines);
+            }
+            MessageBox.Show("File Read.");
+        }
+
+        private void btnScanRestaurantsFromLinks_Click(object sender, EventArgs e)
+        {
+            for (int i = listRestaurants.Count - 1; i >= 0; i--)
+            {
+                this.Text = (i + 1).ToString() + "/" + listRestaurants.Count.ToString();
+                Restaurants restaurants = listRestaurants[i];
+                if (restaurants.URL == baseLink)
+                {
+                    listRestaurants.RemoveAt(i);
+                    continue;
+                }
+                HtmlWeb web = new HtmlWeb();
+                var doc = web.Load(restaurants.URL);
+                string xPath = "/html/body/div/div[3]/div/div[1]/div/div[1]/div[1]/a";
+                if (doc.DocumentNode.SelectNodes(xPath) != null) restaurants.Name = doc.DocumentNode.SelectNodes(xPath).FirstOrDefault().InnerText;
+
+                xPath = "/html/body/div/div[3]/div/div[1]/div/div[1]/div[2]";
+                if (doc.DocumentNode.SelectNodes(xPath) != null) restaurants.Address = doc.DocumentNode.SelectNodes(xPath).FirstOrDefault().InnerText;
+            }
+            MessageBox.Show("Scan Finished.");
+        }
+
+        private void btnWriteCSV_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(restaurantLinksFileName);
+            foreach (var item in listRestaurants)
+            {
+                sw.WriteLine(item.PostalCode + ";" + item.Name + ";" + item.Address);
+            }
+            sw.Close();
+            MessageBox.Show("CSV Written.");
+        }
+
+        private void btnReadCSV_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listRestaurants = new List<Restaurants>();
+            StreamReader sr = new StreamReader(restaurantLinksFileName);
+            while (!sr.EndOfStream)
+            {
+                string lines = sr.ReadLine();
+                string[] line = Regex.Split(lines, ";");
+                listRestaurants.Add(new Restaurants { PostalCode = line[0], Name = line[1], Address = line[2] });
+                listBox1.Items.Add(lines);
+            }
+            MessageBox.Show("CSV Read.");
+        }
+
+        private void btnOpenInBrowser_Click(object sender, EventArgs e)
+        {
+            int start = Convert.ToInt32(nudStart.Value);
+            int finish = Convert.ToInt32(nudFinish.Value);
+            if (start > listRestaurants.Count) start = listRestaurants.Count - 1;
+            if (finish > listRestaurants.Count) finish = listRestaurants.Count - 1;
+            for (int i = start; i < finish; i++)
+            {
+                System.Diagnostics.Process.Start("https://www.google.com/search?q=" + listRestaurants[i].Name);
+            }
+        }
+    }
+
+    class Restaurants
+    {
+        public string URL { get; set; }
+        public string PostalCode { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
     }
 }
